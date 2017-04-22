afd93f59bb32d9bdaa60f5a67cfb6bdf5b69f259
Jenny Chu <jennychu.sd@gmail.com>
2017-04-22 09:09:33 -0700

    still testing routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,8 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
-    app.get(function(req, res) {
+    // If no matching route is found default to home
+    app.use(function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 }
